abd73a8cccc1cc9fbd976562318ae55477f7e23e
Codeminister2137 <kubajaworski99@wp.pl>
2025-04-09 10:19:32 +0200

    Expanding readme and running ruff on files

--- auth_app/permissions.py
+++ auth_app/permissions.py
@@ -1,6 +1,4 @@
 from rest_framework import permissions
-from rest_framework.exceptions import PermissionDenied
-import logging
 
 
 class IsAdminUser(permissions.BasePermission):
--- auth_app/urls.py
+++ auth_app/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path, include
-from .views import CheckLoginView, RegisterView, ChangePasswordView, SpecialResourceView
+from django.urls import path
+from .views import CheckLoginView, RegisterView, ChangePasswordView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
